2ef29760210ae90625c5ce03b3a3ccf1872b58db
turgaymammadov <memmedovturqay871@gmail.com>
2023-11-15 20:03:01 +0400

    yes

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -18,10 +18,10 @@ const About=()=>{
             <div className="row">
                 <div className="col-md-6">
 <h2 className="text-info mt-3">About us</h2>
-<p>At Accessorize we’re dedicated to offering accessories that enable all women to express themselves, helping you feel confident in your everyday.
-<br/><br/>
-Since 1984 we’ve offered affordable accessories that can be stacked and styled, with everything from jewellery and hair accessories to bags and seasonal favourites.Our accessories are an effortless style solution to refresh your look - they’re the accessories that make the outfit.
-We're committed to giving back and supporting the community.<br/> One of the ways we do this is working with charities that support women. We’re also constantly improving our sustainability efforts by making conscious choices with the materials we use and our production processes.</p>
+<p> 
+Accessories are more than adornments; they're storytellers. From the subtle elegance of a well-crafted watch to the bold statement of statement jewelry, each piece is a brushstroke in the canvas of personal style. 
+   We believe that accessories are the bridge between fashion and individuality, elevating every look and defining your unique narrative. 
+</p>
                 </div>
                 <div className="col-md-6 about-img">
                     <img src="https://images.unsplash.com/photo-1547713044-6681f3daa9e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
@@ -29,7 +29,6 @@ We're committed to giving back and supporting the community.<br/> One of the way
             </div>
         </div>
     </div>
-
     </>
 
         )
