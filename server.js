14909ac9d3ace32aca9e1c472e30d12304f8022d
turgaymammadov <memmedovturqay871@gmail.com>
2024-05-24 22:46:18 +0400

    ss

--- server.js
+++ server.js
@@ -24,33 +24,33 @@ app.use(cors({
 }));
 
 
-// app.use("/api/import", ImportData);
-// app.use("/api/products", productRouter)
-// app.use("/api/users", userRouter)
-// app.use("/api/orders",orderRouter)
-// app.use("/api/shop", shopRouter)
-// app.use("/api/config/paypal", (req,res)=>{
-//     res.send(process.env.PAYPAL_CLIENT_ID)
-// })
+app.use("/api/import", ImportData);
+app.use("/api/products", productRouter)
+app.use("/api/users", userRouter)
+app.use("/api/orders",orderRouter)
+app.use("/api/shop", shopRouter)
+app.use("/api/config/paypal", (req,res)=>{
+    res.send(process.env.PAYPAL_CLIENT_ID)
+})
 
 
 
-// if (process.env.NODE_ENV === 'production'){
-// const buildPath=path.resolve(__dirname,'..', "client", "build")
-// app.use(express.static(buildPath))
+if (process.env.NODE_ENV === 'production'){
+const buildPath=path.resolve(__dirname, "client", "build")
+app.use(express.static(buildPath))
 
 // app.use("/images", (req, res, next) => {
 //     res.setHeader("Access-Control-Allow-Origin", "https://accessoriesazz-turqay667s-projects.vercel.app/");
 //     next();
 // });
-// app.get("*", (req, res) => {
-//     res.setHeader("Access-Control-Allow-Credentials","true")
-//     res.sendFile(path.resolve(buildPath, "index.html"));
-// });
-// }
+app.get("*", (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials","true")
+    res.sendFile(path.resolve(buildPath, "index.html"));
+});
+}
 
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/",(req,res)=>{
     res.send("API is running on this port")
