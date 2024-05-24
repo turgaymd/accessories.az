59996affb47e2efd2e81de1f043fc0a146749ac4
turgaymammadov <memmedovturqay871@gmail.com>
2024-05-24 20:38:58 +0400

    yes

--- server/server.js
+++ server/server.js
@@ -16,37 +16,37 @@ connectDatabase({
 const app=express();
 app.use(express.json())
 app.use(cors())
-app.use(cors({
-    origin:'https://accessories-azd.vercel.app'
-}));
-app.use("/api/import", ImportData);
-app.use("/api/products", productRouter)
-app.use("/api/users", userRouter)
-app.use("/api/orders",orderRouter)
-app.use("/api/shop", shopRouter)
-app.use("/api/config/paypal", (req,res)=>{
-    res.send(process.env.PAYPAL_CLIENT_ID)
-})
+// app.use(cors({
+//     origin:'https://accessories-azd.vercel.app'
+// }));
+// app.use("/api/import", ImportData);
+// app.use("/api/products", productRouter)
+// app.use("/api/users", userRouter)
+// app.use("/api/orders",orderRouter)
+// app.use("/api/shop", shopRouter)
+// app.use("/api/config/paypal", (req,res)=>{
+//     res.send(process.env.PAYPAL_CLIENT_ID)
+// })
 
 
-const path=require("path")
-if (process.env.NODE_ENV === 'production'){
-app.use(express.static(path.join(__dirname,'..', "client", "build")))
+// const path=require("path")
+// if (process.env.NODE_ENV === 'production'){
+// app.use(express.static(path.join(__dirname,'..', "client", "build")))
 
 // app.use("/images", (req, res, next) => {
 //     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/accessories");
 //     next();
 // });
-app.get("*", (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials","true")
-    res.sendFile(path.join(__dirname,'..', "client", "build", "index.html"));
-});
-}
+// app.get("*", (req, res) => {
+//     res.setHeader("Access-Control-Allow-Credentials","true")
+//     res.sendFile(path.join(__dirname,'..', "client", "build", "index.html"));
+// });
+// }
 
-app.use(notFound);
-app.use(errorHandler);
+// app.use(notFound);
+// app.use(errorHandler);
 
-app.get("/",(req,res)=>{
+app.get("/yes",(req,res)=>{
     res.send("API is running on this port")
 })
 const PORT=process.env.PORT || 1000;
