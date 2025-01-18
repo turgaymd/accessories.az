d2e68a3c5e881d8eaabf5335b7f0bb79b7aeecaa
turgaymammadov <memmedovturqay@871gmail.com>
2025-01-18 17:01:24 +0400

    s

--- client/src/ApiContext.js
+++ client/src/ApiContext.js
@@ -1,9 +1,9 @@
 import { createContext, useContext } from "react"
 export const APiContext=createContext(null)
 
-const apiUrl=process.env.NODE_ENV==='production' ? 'https://accessories-az-ten.vercel.app/api' : 'http://localhost:8000/api'
+const apiUrl=process.env.NODE_ENV==='production' ? 'https://accessories-az-ten.vercel.app/' : 'http://localhost:8000/api'
 
-const ApiProvider=(props)=>{
+const ApiProvider=(props)=>{s
     return(
 <APiContext.Provider value={{apiUrl}}>
     {props.children}
